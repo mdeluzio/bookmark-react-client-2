ae0c2edb1cb3975ee0fe4d5b4f788aa89ebc0f7c
Mark DeLuzio <deluziomark@gmail.com>
2020-02-22 17:31:54 -0500

    changed AddBookmarks styles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
-//import AddBookmark from './AddBookmark/AddBookmark';
+import AddBookmark from './AddBookmark/AddBookmark';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        
+        <AddBookmark />
       </div>
     );
   };
