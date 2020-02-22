00e7e2f328b9eaf67d9c944fe947d46e243fdeb6
Mark DeLuzio <deluziomark@gmail.com>
2020-02-22 17:24:33 -0500

    created static version on AddBookmark component with basic styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
+//import AddBookmark from './AddBookmark/AddBookmark';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-
+        
       </div>
     );
   };
