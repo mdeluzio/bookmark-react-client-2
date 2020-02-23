9ef9f8e632a4b29690cf9cca8178a3d228a1f0fa
Mark DeLuzio <deluziomark@gmail.com>
2020-02-22 19:29:55 -0500

    updated to use state and replaced dummy data with data from an api

--- src/App.js
+++ src/App.js
@@ -3,27 +3,58 @@ import './App.css';
 import AddBookmark from './AddBookmark/AddBookmark';
 import BookmarkApp from './BookmarkApp/BookmarkApp';
 
-const bookmarks = [
-  {
-  title:"Google",
-  url:"http://www.google.com", 
-  rating:"3", 
-  description:"No evil"
-  },
-  {
-    title:"Google",
-    url:"http://www.google.com", 
-    rating:"3", 
-    description:"No evil"
-  }
-];
-
 class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      bookmarks: [],
+      showAddForm: false
+    };
+  };
+
+  componentDidMount() {
+    const url = 'https://tf-ed-bookmarks-api.herokuapp.com/v3/bookmarks';
+    const options = {
+      method: 'GET',
+      headers: {
+        "Authorization": "Bearer $2a$10$ABbBxBUwD.DF7DFP8L8L7eSakrR2xM.Is1l0Kh8teoyTzxY5OuAHS",
+        "Content-Type": "application/json"
+      }
+    };
+
+    fetch(url, options)
+      .then(response => {
+        if(!response.ok) {
+          throw new Error('Something went wrong, please try again later.')
+        }
+        return response
+      })
+      .then(response => response.json())
+      .then(data => {
+        console.log(data)
+        this.setState({
+          bookmarks: data,
+          error: null
+        });
+      })
+      .catch(err => {
+        this.setState({
+          error: err.message
+        });
+      });
+  }
+
+
   render() {
+    const page = this.state.showAddForm
+      ? <AddBookmark />
+      : <BookmarkApp bookmarks={this.state.bookmarks} />
+    ;
+
     return (
       <div className="App">
-        <AddBookmark />
-        <BookmarkApp bookmarks={bookmarks} />
+        {page}
       </div>
     );
   };
